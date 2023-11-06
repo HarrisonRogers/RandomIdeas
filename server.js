05fcaf44f97871b928d4ff6538e085743aa689ad
Harrison Rogers <harrisonrogers@Harrisons-MacBook-Pro-2.local>
2023-11-06 14:40:39 +1300

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const port = 5500;
+
+const app = express();
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Under the starry night sky, the curious cat chased a firefly through the enchanting forest, guided by the gentle whispers of the wind.',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2023-03-12',
+  },
+  {
+    id: 2,
+    text: 'As the sun dipped below the horizon, a rainbow appeared, casting a colorful bridge across the tranquil lake, where a lone swan gracefully glided.',
+    tag: 'Inventions',
+    username: 'BruceBanner',
+    date: '2023-04-24',
+  },
+  {
+    id: 3,
+    text: '"Amid the bustling city streets, a street musician played a soulful melody on his saxophone, capturing the hearts of passersby as the sun set in the background."',
+    tag: 'Gaming',
+    username: 'MikeMyers',
+    date: '2021-12-30',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+// Get all Ideas
+app.get('/api/ideas', (req, res) => {
+  res.json({ success: true, data: ideas });
+});
+
+app.get('/api/ideas/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  res.json({ success: true, data: idea });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
